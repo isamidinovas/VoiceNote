af36bf03097b911fdb2e3a1ea94747826ff7e55c
symbat <symbatisamidinova@gmail.com>
2025-02-10 18:23:41 +0600

    add: animations to icons

--- src/containers/Content/Content.jsx
+++ src/containers/Content/Content.jsx
@@ -2,26 +2,51 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import { FaMicrophone } from "react-icons/fa";
 import { VscDebugStart } from "react-icons/vsc";
+import { VscDebugPause } from "react-icons/vsc";
 export const Content = () => {
   const [isRecording, setIsRecording] = useState(false);
+  const [isDebugging, setIsDebugging] = useState(false);
+  const toggleIcon = () => {
+    setIsDebugging(!isDebugging); // Переключает состояние
+  };
   return (
-    <div className="flex flex-col items-center gap-12 ">
-      <h1 className="text-2xl mt-7">Voice Note</h1>
-      <div className="w-[100%] max-w-[800px] bg-slate-200 h-[300px] rounded-xl"></div>
+    <div className="flex flex-col items-center gap-6 sm:gap-12 mt-4 ml-5 mr-5 mb-10 ">
+      <h1 className="text-2xl mt-7 font-bold">Voice Note</h1>
+      <div className="w-[100%] max-w-[800px]  bg-gray-900 min-h-[200px] rounded-xl p-5 text-left text-white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+      </div>
+
       <motion.div
-        className="p-6 bg-gray-800 rounded-full text-white cursor-pointer"
-        // onMouseDown={startRecording} // Начало при нажатии
-        // onMouseUp={stopRecording} // Остановка при отпускании
-        // onTouchStart={startRecording} // Для мобильных устройств
-        // onTouchEnd={stopRecording}
-        animate={isRecording ? { opacity: [1, 0.5, 1] } : {}}
-        transition={{ duration: 0.5, repeat: Infinity }}
+        whileTap={{
+          scale: 1.4, // Увеличение при нажатии
+          opacity: 0.5, // Прозрачность при нажатии
+        }}
+        transition={{ duration: 0.3 }} // Плавный переход
+        className="p-2 cursor-pointer"
       >
-        <FaMicrophone className="text-4xl" />
+        <FaMicrophone size={48} className="text-gray-900" />
       </motion.div>
-      {/* <FaMicrophone className="text-4xl cursor-pointer" /> */}
       <div className="w-[300px] flex justify-between items-center">
-        <VscDebugStart className="text-5xl border-2 border-black rounded-full p-1" />
+        <div
+          className="text-5xl border-2 border-gray-900 rounded-full p-1
+          cursor-pointer"
+          onClick={toggleIcon}
+        >
+          {isDebugging ? (
+            <VscDebugPause className="text-gray-900" />
+          ) : (
+            <VscDebugStart className="text-gray-900" />
+          )}{" "}
+        </div>
+
+        {/* Меняем иконку */}
         <p>ЗАПИСЬ</p>
       </div>
     </div>
